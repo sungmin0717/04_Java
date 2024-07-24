bd6209bdc8afb6c2a195ecfbc167542e2322f112
DESKTOP-IEKEC4V\김성민 <sungmin48601@gmail.com>
2024-07-24 22:01:29 +0900

    0724 학원수업 home

---
+++ 07_inheritance/src/dto/Child2.java
@@ -0,0 +1,11 @@
+package dto;
+
+public class Child2 extends Parent{
+
+	
+	@Override
+	public String toString() {
+		return "[Child22222]" + super.toString();
+		
+	}
+}
---
+++ 07_inheritance/src/dto/FinalChild.java
@@ -0,0 +1,6 @@
+package dto;
+
+//final 클래스 상속 불가 확인
+public class FinalChild /* exetends FinalParent */
+	extends Parent, Child3{
+}
